c8465f8259b8f4d0e1a3f83b346f36231d3b53e1
Rafael Sá <sanunesrafael@gmail.com>
2022-09-02 00:27:14 -0300

    atualizando com try-with-resources

--- courseFileReaderBufferedReader/src/application/Program.java
+++ courseFileReaderBufferedReader/src/application/Program.java
@@ -9,12 +9,11 @@ public class Program {
     public static void main(String[] args) {
 
         String path = "c:\\temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        /*para melhor instanciar nossas streams devemos instancia direto no bloco try. Para tanto,
+        * declaro o BufferedReader e faz ele receber um new BufferedReader onde, no seu construtor,
+        * já colocamos o new FileReader que recebe o path para abrir o arquivo*/
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
             while (line != null) {
@@ -23,19 +22,6 @@ public class Program {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
